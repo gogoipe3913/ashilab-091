2f16e8c3d93bfe6ebdc81846748dda9ed9130d5b
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-03-25 01:39:05 +0900

    Reservationを追加

---
+++ src/components/organisms/Reservation/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import classNames from "classnames";
+import mainImage from "/public/images/image4.png";
+import style from "./style.module.scss";
+
+type ReservationProps = {
+  className?: string;
+};
+
+const Reservation: React.FC<ReservationProps> = ({ className = "" }) => (
+  <div id="reservation" className={classNames(style.Reservation, className)}>
+    <p className={style.Reservation__title}>【予約表】</p>
+    <div className={style.Reservation__heading}>
+      <img
+        src={mainImage}
+        alt="フードイベントのイメージ画像"
+        className={style.Reservation__headingImage}
+      />
+      <h2 className={style.Reservation__headingText}>
+        <p>ご予約はこちらまで</p>
+        <p className={style.Reservation__headingSubtitle}>Reservation table</p>
+      </h2>
+    </div>
+  </div>
+);
+
+export default Reservation;
