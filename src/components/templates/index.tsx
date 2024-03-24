4987af5063c936ffbb2a3dc22a64c5776bde5737
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-03-25 01:39:46 +0900

    Templatesを修正

--- src/components/templates/index.tsx
+++ src/components/templates/index.tsx
@@ -3,6 +3,14 @@ import Top from "../organisms/Top";
 import ReserveButton from "../organisms/ReserveButton";
 import OnlineStoreLink from "../organisms/OnlineStoreLink";
 import About from "../organisms/About";
+import LocationShooting from "../organisms/LocationShooting";
+import style from "./style.module.scss";
+import FoodEvents from "../organisms/FoodEvents";
+import BusinessParty from "../organisms/BusinessParty";
+import CookingClass from "../organisms/CookingClass";
+import Footer from "../organisms/Footer";
+import Access from "../organisms/Access";
+import Reservation from "../organisms/Reservation";
 
 const Templates: React.FC = () => {
   return (
@@ -11,6 +19,13 @@ const Templates: React.FC = () => {
       <Top />
       <OnlineStoreLink />
       <About />
+      <LocationShooting className={style.Templates__locationShooting} />
+      <FoodEvents className={style.Templates__foodEvents} />
+      <BusinessParty className={style.Templates__businessParty} />
+      <CookingClass className={style.Templates__cookingClass} />
+      <Access className={style.Templates__access} />
+      <Reservation className={style.Templates__reservation} />
+      <Footer />
     </>
   );
 };
