0239113a089614b55232e1bce15db863cc32a40d
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-03-25 01:12:36 +0900

    Food Eventsを追加

---
+++ src/components/organisms/FoodEvents/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import classNames from "classnames";
+import mainImage from "/public/images/image4.png";
+import style from "./style.module.scss";
+
+type FoodEventsProps = {
+  className?: string;
+};
+
+const FoodEvents: React.FC<FoodEventsProps> = ({ className = "" }) => (
+  <div id="foodEvents" className={classNames(style.FoodEvents, className)}>
+    <p className={style.FoodEvents__title}>【フードイベント】</p>
+    <div className={style.FoodEvents__heading}>
+      <img
+        src={mainImage}
+        alt="フードイベントのイメージ画像"
+        className={style.FoodEvents__headingImage}
+      />
+      <h2 className={style.FoodEvents__headingText}>
+        <p>「作る」「食べる」＋αの</p>
+        <p>多様性空間</p>
+        <p className={style.FoodEvents__headingSubtitle}>Food Events</p>
+      </h2>
+    </div>
+    <p className={style.FoodEvents__description}>
+      ワインの試飲会や日本酒の利き酒、新レシピの発表会、シェフが腕をふるうランチ付きイベントなど、
+      様々なフードイベントにご活用いただけます。レイアウトを変更すれば立食・着席どちらのスタイルも可能で、
+      特に料理を発信する企業PRイベントにはもってこい。キッチンスタジオを自由に使い、多様性と価値がある
+      フードイベントを仕掛けられます。
+    </p>
+    <p className={style.FoodEvents__descriptionEn}>
+      Wine tastings, sake tastings, new recipe presentations, events with lunch
+      where chefs show off their skills, etc. It can be used for various food
+      events. By changing the layout, you can have both standing and seated
+      dining styles. It is especially suitable for corporate PR events promoting
+      food. Use the kitchen studio freely, have variety and value You can set up
+      a food event.
+    </p>
+  </div>
+);
+
+export default FoodEvents;
