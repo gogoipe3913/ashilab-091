8e5af32c7d34a008f3f9bc10c10a0904cc55cee9
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-03-27 01:21:46 +0900

    scriptタグの内容の修正

--- src/main.tsx
+++ src/main.tsx
@@ -7,18 +7,28 @@ import { Helmet } from "react-helmet";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Helmet>
-      <script>
-        {`
-          (function(d) {
-            var config = {
-              kitId: 'num5eul',
-              scriptTimeout: 3000,
-              async: true
-            },
-            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-          })(document);
-        `}
-      </script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(d) {
+              var config = {
+                kitId: 'num5eul',
+                scriptTimeout: 3000,
+                async: true
+              },
+              h=d.documentElement,
+              t=setTimeout(function(){
+                h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";
+              },
+              config.scriptTimeout),
+              tk=d.createElement("script"),
+              f=false,
+              s=d.getElementsByTagName("script")[0],
+              a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+            })(document);
+          `,
+        }}
+      />
       <link rel="stylesheet" href="https://use.typekit.net/imb0qps.css"></link>
     </Helmet>
     <App />
