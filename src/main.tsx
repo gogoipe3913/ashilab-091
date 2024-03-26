d42bab5aa9d1c6dcdb918060499dbc7c8073c00b
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-03-27 01:12:21 +0900

    react-helmetでscriptタグを追加する

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.scss";
+import { Helmet } from "react-helmet";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Helmet>
+      <script>
+        {`
+          (function(d) {
+            var config = {
+              kitId: 'num5eul',
+              scriptTimeout: 3000,
+              async: true
+            },
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+          })(document);
+        `}
+      </script>
+      <link rel="stylesheet" href="https://use.typekit.net/imb0qps.css"></link>
+    </Helmet>
     <App />
   </React.StrictMode>
 );
