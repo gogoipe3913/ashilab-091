59fb7d587ddb83e0adbd567b71a710b41adb9160
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-03-26 18:16:44 +0900

    Sp用のグローバルナビゲーションを追加

---
+++ src/components/organisms/SpNavigation/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./style.module.scss";
+
+const SpNavigation: React.FC = () => {
+  return (
+    <div className={style.SpNavigation}>
+      <a href="">RESERVE</a>
+      <button></button>
+    </div>
+  );
+};
+
+export default SpNavigation;
