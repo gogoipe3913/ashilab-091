ad3c91bd086f342e086562718968d6fb444677f0
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-03-28 12:57:10 +0900

    SpNavigationの内容を追加

--- src/components/organisms/SpNavigation/index.tsx
+++ src/components/organisms/SpNavigation/index.tsx
@@ -1,11 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./style.module.scss";
+import classNames from "classnames";
+import { NAVIGATION_ITEMS } from "../Top";
+import LogoMain from "../../atoms/LogoMain";
 
 const SpNavigation: React.FC = () => {
+  const [isDisplayedNavigation, setIsDisplayedNavigation] = useState(false);
+
   return (
     <div className={style.SpNavigation}>
-      <a href="">RESERVE</a>
-      <button></button>
+      <a href="" className={style.SpNavigation__reserve}>
+        RESERVE
+      </a>
+      <button
+        className={style.SpNavigation__hamburger}
+        onClick={() => {
+          setIsDisplayedNavigation(!isDisplayedNavigation);
+        }}
+      >
+        <span
+          className={classNames(
+            style.SpNavigation__hamburgerLine,
+            isDisplayedNavigation
+              ? style["SpNavigation__hamburgerLine--top"]
+              : ""
+          )}
+        />
+        <span
+          className={classNames(
+            style.SpNavigation__hamburgerLine,
+            isDisplayedNavigation
+              ? style["SpNavigation__hamburgerLine--middle"]
+              : ""
+          )}
+        />
+        <span
+          className={classNames(
+            style.SpNavigation__hamburgerLine,
+            isDisplayedNavigation
+              ? style["SpNavigation__hamburgerLine--bottom"]
+              : ""
+          )}
+        />
+      </button>
+      {/* ナビゲーションコンテンツ */}
+      <div
+        className={classNames(
+          style.SpNavigationContents,
+          isDisplayedNavigation ? style["SpNavigationContents--displayed"] : ""
+        )}
+      >
+        <div className={style.SpNavigationContents__top}>
+          <p className={style.SpNavigationContents__address}>
+            2-6-5, Komagata, Taito Ku, Tokyo To, 111-0043, Japan
+          </p>
+          <div className={style.SpNavigationContents__logoWrapper}>
+            <LogoMain />
+          </div>
+        </div>
+        <p className={style.SpNavigationContents__copyright}>©︎2024 ashi lab</p>
+        <nav className={style.SpNavigationContents__nav}>
+          <ul className={style.SpNavigationContents__navItems}>
+            {NAVIGATION_ITEMS.map((item, index) => (
+              <li key={index} className={style.SpNavigationContents__navItem}>
+                {index === 0 ? null : (
+                  <span
+                    className={style.SpNavigationContents__navItemSeparator}
+                  />
+                )}
+                <a
+                  href={`#${item.id}`}
+                  className={style.SpNavigationContents__navLink}
+                  onClick={() => {
+                    setIsDisplayedNavigation(false);
+                  }}
+                >
+                  {item.displayText}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
