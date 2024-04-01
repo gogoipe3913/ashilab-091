4d3d2227e2d20f6b69026f8e3d7358c0cfebdaa5
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-04-02 03:44:30 +0900

    予約ページをiframeで埋め込む

--- src/components/organisms/Reservation/index.tsx
+++ src/components/organisms/Reservation/index.tsx
@@ -1,27 +1,33 @@
 import React from "react";
 import classNames from "classnames";
-import mainImage from "/public/images/image4.png";
 import style from "./style.module.scss";
 
 type ReservationProps = {
   className?: string;
 };
 
-const Reservation: React.FC<ReservationProps> = ({ className = "" }) => (
-  <div id="reservation" className={classNames(style.Reservation, className)}>
-    <p className={style.Reservation__title}>【予約表】</p>
-    <div className={style.Reservation__heading}>
-      <img
-        src={mainImage}
-        alt="フードイベントのイメージ画像"
-        className={style.Reservation__headingImage}
-      />
-      <h2 className={style.Reservation__headingText}>
-        <p className={style.Reservation__headingTitle}>ご予約はこちらまで</p>
-        <p className={style.Reservation__headingSubtitle}>Reservation table</p>
-      </h2>
+const Reservation: React.FC<ReservationProps> = ({ className = "" }) => {
+  return (
+    <div id="reservation" className={classNames(style.Reservation, className)}>
+      <p className={style.Reservation__title}>【予約表】</p>
+      <div className={style.Reservation__heading}>
+        <div className={style.Reservation__reservationPageFrame}>
+          <iframe
+            src="https://squareup.com/appointments/book/8rr5l1bshnapry/LJ0H72RBE83AV/services"
+            width="100%"
+            height="100%"
+            frameBorder={0}
+          />
+        </div>
+        <h2 className={style.Reservation__headingText}>
+          <p className={style.Reservation__headingTitle}>ご予約はこちらまで</p>
+          <p className={style.Reservation__headingSubtitle}>
+            Reservation table
+          </p>
+        </h2>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Reservation;
