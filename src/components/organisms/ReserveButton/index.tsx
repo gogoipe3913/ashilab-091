5a4e38a58b3d92c5d8c1ab35000e889eac7f1b43
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-04-02 00:26:59 +0900

    予約ボタンにhrefを仕込む

--- src/components/organisms/ReserveButton/index.tsx
+++ src/components/organisms/ReserveButton/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import style from "./style.module.scss";
 
+const RESERVE_LINK =
+  "https://squareup.com/appointments/book/8rr5l1bshnapry/LJ0H72RBE83AV/start";
+const REL = "nofollow";
+
 const ReserveButton: React.FC = () => (
-  <button className={style.ReserveButton}>
+  <a href={RESERVE_LINK} rel={REL} className={style.ReserveButton}>
     <span className={style.ReserveButton__text}>RESERVE</span>
-  </button>
+  </a>
 );
 
 export default ReserveButton;
