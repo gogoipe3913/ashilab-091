fba18b9edc1135978d557d336116ed913f681720
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-08-28 16:04:11 +0900

    Reservationを修正

--- src/components/organisms/Reservation/index.tsx
+++ src/components/organisms/Reservation/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import classNames from "classnames";
 import style from "./style.module.scss";
 
+const RESERVE_LINK = "https://coubic.com/ashilab/2371791";
+const REL = "nofollow";
+const TARGET = "_blank";
+
 type ReservationProps = {
   className?: string;
 };
@@ -15,17 +19,24 @@ const Reservation: React.FC<ReservationProps> = ({ className = "" }) => {
     <div id="reservation" className={classNames(style.Reservation, className)}>
       <p className={style.Reservation__title}>【予約表】</p>
       <div className={style.Reservation__heading}>
-        <div
+        <a
+          href={RESERVE_LINK}
+          rel={REL}
+          target={TARGET}
           className={style.Reservation__reservationPageFrame}
-          onWheel={handleWheel}
+          onClick={(event) => {
+            event.stopPropagation();
+          }}
         >
           <iframe
             src="https://coubic.com/ashilab/services#pageContent"
             width="100%"
             height={700}
             frameBorder={0}
+            onWheel={handleWheel}
+            className={style.Reservation__frameBody}
           />
-        </div>
+        </a>
         <h2 className={style.Reservation__headingText}>
           <p className={style.Reservation__headingTitle}>ご予約はこちらまで</p>
           <p className={style.Reservation__headingSubtitle}>
