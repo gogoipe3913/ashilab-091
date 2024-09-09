2361cff294edff65671c48ccce12e07ae891d23b
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2024-09-09 21:51:57 +0900

    オンラインサイトのURLを追加

--- src/components/organisms/OnlineStoreLink/index.tsx
+++ src/components/organisms/OnlineStoreLink/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import style from "./style.module.scss";
 
 const OnlineStoreLink: React.FC = () => (
-  <a href="" className={style.OnlineStoreLink}>
+  <a
+    href="https://ashi-lab-shop.myshopify.com/"
+    target="_blank"
+    className={style.OnlineStoreLink}
+  >
     <span className={style.OnlineStoreLink__text}>
       <span>ONLINE</span>
       <span>STORE</span>
